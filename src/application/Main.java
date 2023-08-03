2ad589118b8459ec2f96dbdb17816471bbf27062
Andrieli Mendes <andrieli.m31@aluno.ifsc.edu.br>
2023-08-03 15:57:20 -0300

    novo Projeto

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,34 +1,29 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
-
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//configurando alguns dados da tela 
-			primaryStage.setTitle("Login");
-			primaryStage.resizableProperty().setValue(Boolean.FALSE);
-			
-
-			// carregar arquivo da tela scenebuilder
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/visao/telaLogin.fxml"));
-			Parent arquivoXML = loader.load();
-			
-			
-			
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/visao/Test.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("/visao/Test.fxml").toExternalForm());
+			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+ 
